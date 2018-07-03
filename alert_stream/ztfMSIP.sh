03e627463b2e52f2af56559bfe5fab4ab49a5e7f
Zach.Golkhou <golkhou@gmail.com>
2018-07-02 21:05:30 -0700

    Update ztfMSIP.sh

--- alert_stream/ztfMSIP.sh
+++ alert_stream/ztfMSIP.sh
@@ -18,15 +18,13 @@
 #tarName=$3
 topic_name=`printf '^ztf_%(%Y%m%d)T_programid1' -1`
 tarName=`printf 'ztf_public_%(%Y%m%d)T' -1`
-#group_name=`printf 'MSIP%(%Y%m%d)T' -1`
 group_name=catchingMSIPalerts
 
-logpth='/epyc/data/ztfDB/log/logMSIPStatus.txt'
 pth='/epyc/data/ztfDB/pro_msip/alert_stream'
 pth0='/epyc/data/ztf/alerts'
 
 export PATH="/epyc/opt/anaconda/bin:$PATH"
-export PYTHONPATH="/epyc/data/ztfDB/pro/alert_stream/python:$PYTHONPATH"
+export PYTHONPATH="./alert_stream/python:$PYTHONPATH"
 
 if [ ! -f $pth0/public/${tarName}.tar.gz ] ; then
         # step:1
@@ -40,17 +38,10 @@ if [ ! -f $pth0/public/${tarName}.tar.gz ] ; then
         # step:2
         # tar --> tar.gz
         #    
-        #if [ -f /epyc/data/ztfMSIP/${tarName}.tar ] ; then
-        #    gzip /epyc/data/ztfMSIP/${tarName}.tar
-            #
-        #    if [ -f /epyc/data/ztfMSIP/${tarName}.tar.gz ] ; then
-        #        echo "# gunzip tar file: DONE" >> $logpth
-        #    else
-        #        echo "# gunzip tar file: Failed" >> $logpth
-        #    fi
-        #fi
+        if [ -f ./${tarName}.tar ] ; then
+            gzip ./${tarName}.tar
+        fi
 
 else
-	echo \# topic: $topic_name has already been read. >> $logpth
+	echo topic: $topic_name has already been read.
 fi
-
