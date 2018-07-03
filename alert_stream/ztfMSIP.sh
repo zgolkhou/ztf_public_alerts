433b286576d700a00e57b5b5f800963a5cb8f87e
Zach.Golkhou <golkhou@gmail.com>
2018-07-02 21:19:28 -0700

    Update ztfMSIP.sh

--- alert_stream/ztfMSIP.sh
+++ alert_stream/ztfMSIP.sh
@@ -16,26 +16,33 @@
 #topic_name=$1
 #group_name=$2
 #tarName=$3
-topic_name=`printf '^ztf_%(%Y%m%d)T_programid1' -1`
+topic_name=`printf 'ztf_%(%Y%m%d)T_programid1' -1`
 tarName=`printf 'ztf_public_%(%Y%m%d)T' -1`
 group_name=catchingMSIPalerts
-
-pth='/epyc/data/ztfDB/pro_msip/alert_stream'
-pth0='/epyc/data/ztf/alerts'
 
 export PATH="/epyc/opt/anaconda/bin:$PATH"
 export PYTHONPATH="./alert_stream/python:$PYTHONPATH"
 
-if [ ! -f $pth0/public/${tarName}.tar.gz ] ; then
+if [ ! -f ./${tarName}.tar.gz ] ; then
         # step:1
         # reading a topic and creating topic.tar file from all alerts.avro
         #
 	echo -e "\n\n-------------------------------\n\n"
 	echo \# topic name: $topic_name
 	#
-	python $pth/bin/catchMSIPStream.py ${topic_name} --group $group_name --tarName ${tarName}
+	python ./alert_stream/bin/catchMSIPStream.py ${topic_name} --group $group_name --tarName ${tarName}
         #
-        # step:2
+	# step:2
+        # avro --> csv
+        #
+        if [ -f ./${tarName}.tar ] ; then
+            echo -e "# time: $(date) \n"
+            python ./alert_stream/bin/avro_csv.py ${collab_tarName}
+            echo -e "# time: $(date) \n"
+        else
+            echo "# tarball file does not exist."
+        fi
+        # step:3
         # tar --> tar.gz
         #    
         if [ -f ./${tarName}.tar ] ; then
